0b064a2b1fa1519d294e05ea4fc6ec2189553cf1
danielagustbjornsson <70696534+danielagustbjornsson@users.noreply.github.com>
2020-09-17 10:14:54 +0000

    Update TileTraveller.py

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,6 +1,10 @@
-# hello test
-#hae
+
 # def way_choser
     # program það takes in what ways are alowed and the room that there in lets the user choose and sents room there in then
 # def room_checker
+    # if room == x
+        # set North = x
+        # set South = x
+        # set East = x
+        # set West = x
     # check what room they are in and showes what moves are alowed
