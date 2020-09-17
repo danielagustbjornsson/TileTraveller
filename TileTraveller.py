fca26106ee00fd62a53421929e898ab62fb9c74e
danielagustbjornsson <70696534+danielagustbjornsson@users.noreply.github.com>
2020-09-17 10:54:46 +0000

    room way checker done

--- TileTraveller.py
+++ TileTraveller.py
@@ -8,3 +8,37 @@
         # set East = x
         # set West = x
     # check what room they are in and showes what moves are alowed
+
+def room_checker (room):
+    North = False
+    South = False
+    East = False
+    West = False
+    if room == "1,1":
+        North = True
+    elif room == "1,2":
+        North = True
+        South = True
+        East = True
+    elif room == "1,3":
+        South = True
+        East = True
+    elif room == "2,1":
+        North = True
+    elif room == "2,2":
+        South = True
+        West = True
+    elif room == "2,3":
+        West = True
+        East = True
+    elif room == "3,1":
+        North = True
+        # wining room
+    elif room == "3,2":
+        North = True
+        South = True
+    elif room == "3,3":
+        West = True
+        South = True
+    return North, South, East, West
+        
