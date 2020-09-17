a521686f5df180c0289c372fd4a03ff9d6f9b068
Hanna <71434363+Hanna444@users.noreply.github.com>
2020-09-17 09:47:29 +0000

    Update TileTraveller.py
    prufa

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,2 +1,6 @@
 # hello test
 #hae
+# def way_choser
+    # program það takes in what ways are alowed and the room that there in lets the user choose and sents room there in then
+# def room_checker
+    # check what room they are in and showes what moves are alowed
